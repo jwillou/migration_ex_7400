b05ee44f80df6fbfe4eca91ffe463469f662bf62
Janna Willoughby <jwilloughby@auburn.edu>
2021-08-19 08:04:54 -0500

    add exercise overview

---
+++ migration_example.R
@@ -0,0 +1,6 @@
+setwd("/Users/jannawilloughby/GDrive/ABMs/migration_ex_7400/")
+
+#Simulate movement between two populations that have 100 individuals each. Exactly one individual should move from 
+#pop A to pop B and from pop B to pop A each generation for 50 generations. 
+
+#set up pop A...
